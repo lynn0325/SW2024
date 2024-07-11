e01d58dcc51db20cbe2dcd74d96668e600056358
Sola0822 <dldpthf2003@naver.com>
2024-07-11 22:50:06 +0900

    로그인/회원가입 + 게시판 추가

--- asset/tab.js
+++ asset/tab.js
@@ -1,9 +1,10 @@
 function openTab(evt, tab) {
-	var i, tabcontent, tablinks, currentTab, isCurrentActive;
+	var i, tabcontent, tablinks, currentTab, maincontent, isCurrentActive;
 	
 	tabcontent = document.getElementsByClassName("content__inner"); // 인덱스 내부 본문에 해당하는 클래스 이름
 	tablinks = document.getElementsByClassName("tab"); // 인덱스에 해당하는 클래스 이름
 	currentTab = document.getElementById(tab); // 인덱스 내부 본문에 해당하는 아이디
+	maincontent = document.querySelector(".main"); //메인 페이지 선택
     isCurrentActive = evt.currentTarget.classList.contains("active"); // 클릭된 탭에 active가 있었는가 확인
 	
 	for (i = 0; i < tabcontent.length; i++) { // 탭 내용 숨김
@@ -16,9 +17,11 @@ function openTab(evt, tab) {
 	
 	if (isCurrentActive) { // 이미 활성화된 경우 -> 본문 내용 안보이게 하기
 		currentTab.style.display = "none";
+		maincontent.style.display = "block";
 	} else {
 		currentTab.style.display = "block"; // 본문 내용 보임
 		evt.currentTarget.className += " active"; // active 클래스 추가
+		maincontent.style.display = "none";
 	}
 
 }
